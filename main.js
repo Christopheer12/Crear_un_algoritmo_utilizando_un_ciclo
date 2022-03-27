5b30ddb10033d72980c888159a2fc5774e144113
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-03-27 02:52:59 -0300

    master

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+/* ejemplo de for y while crea un algoritmo que repira un bloque de instrucciones*/
 alert("hola");
+const espacio = " ";
+let numeroA = 1;
+let numeroB = 2;
+let numeroC = numeroA + numeroB;
+console.log(numeroC)
