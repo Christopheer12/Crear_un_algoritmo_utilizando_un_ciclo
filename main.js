761b8cc6c1414fe2c6e06e4c15bda7d133b9fd77
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-03-28 21:47:56 -0300

    master

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 /* ejemplo de for y while crea un algoritmo que repira un bloque de instrucciones*/
-alert("hola");
-const espaciO = " ";
-let numeroA = 1;
-let numeroB = 2;
-let numeroC = numeroA + numeroB;
-console.log(numeroC);
 
-let palabraA = "Coder";
-let palabrab ="house";
-let palabraC = palabraA + espaciO + palabrab ;
-console.log(palabraC);
 
 /* Promt*/
 let verificacion = parseInt(prompt("¿que edad tienes?"));
@@ -25,4 +15,8 @@ else if (verificacion >=18 && verificacion <=20 ){
 else if (verificacion >=0 && verificacion <=17
      ){
     alert("JAJAJAJA, eres un BEBE CURIOSO");
+}
+
+for (let i = 0;  i < 10; i++){
+    console.log(i);
 }
